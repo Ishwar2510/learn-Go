7cd16b559d9277ea85c9c0e3b7ceb116d0999abf
ishwarKumar <ishwar2510@gmail.com>
2023-12-30 14:52:49 +0530

    This changes made one main packge and one module the main module is dependent on the second module.

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello Niranjana")
 	fmt.Println(quote.Go())
+	var message string
+	message, err := greetings.Hello("Gayatri")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+	message, err = greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
